31538557d2996d1c7d1d4af34541fb71984d1b15
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-02 01:37:41 -0700

    commit 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+/* This function prints all numbers in base 10 */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		printf("%d\n", num);
+		num++;
+	}
+
+	return (0);
+}
