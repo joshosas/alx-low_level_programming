29fcea7090f93249b69e70ecdeb047bd3f06f1b3
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-05 16:18:45 -0700

    commit 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * function prints alphabets in lower case
+ * Print alphabets - function prints alphabets in lower case
  *
  * Return: Always 0
  */
+
 /* function prints alphabets */
 void print_alphabet(void)
 {
