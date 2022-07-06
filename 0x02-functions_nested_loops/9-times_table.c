39e3abae9588698c5ccaacba0ae8e51f26538e5c
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-06 04:50:21 -0700

    commit 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table 
+ *
+ * Return: Always 0
+ */
+/* times_table: functiion prints times table zero to nine */
+void times_table(void)
+{
+	int x = 0;
+	int y;
+	int mul;
+	while (x <= 9)
+	{
+		y = 0;
+		while (y <= 9)
+		{
+			mul = x * y;
+			if (y == 0)
+			{
+				_putchar('0' + mul);
+			}
+			else if (mul < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + mul);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
