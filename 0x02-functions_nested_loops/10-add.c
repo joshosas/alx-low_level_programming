67263f4a9f1877c6dbbd101171da61bab4fa811d
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-06 06:40:01 -0700

    commit 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - return sum of parameters
+ * @x: int 1
+ * @y: int 2
+ * Return: Always 0
+ */
+
+/* add: function sums up integers */
+int add(int x, int y)
+{
+	return ( x+ y);
+}
