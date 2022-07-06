cde4c6d9a38b4689e699a032a073ea82948a6812
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-05 21:14:41 -0700

    commit 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _Islower - checks if lower case alphabet is loweer
- *
+ * _Islower:  checks if alphabet is a lower case letter
+ * @c: character to check
  * Return: 0 or 1
  */
 int _islower(int c)
