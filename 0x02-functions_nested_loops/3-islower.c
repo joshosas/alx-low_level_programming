71b43a0cb702c524adbf9179d70fd73d71771165
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-05 21:31:48 -0700

    commit 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _Islower:  checks if alphabet is a lower case letter
+ * _isalpha - function checks for lower case letters
  * @c: character to check
  * Return: 0 or 1
  */
+/* _isalpha: function checks lower case for alphabets */
 int _islower(int c)
 {
 	return (c >= 97 && c <= 122);
