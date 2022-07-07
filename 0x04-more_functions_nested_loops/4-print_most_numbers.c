08213e5eabdb2d757b7ae4807b297e71fa96c422
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-07 15:20:41 -0700

    commit 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_modt_numbers - prints numbers 0 to 9 except 2 and 4
+ * 
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	char a = 0;
+
+	while (a <= 9)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar('0' + a);
+		}
+		a++;
+	}
+	_putchar('\n');
+
+}
