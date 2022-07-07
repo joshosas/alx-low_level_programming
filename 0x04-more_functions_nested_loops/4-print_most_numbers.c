9a93f0c9c444f242661bd2aba093b4d63ad5f228
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-07 15:39:15 -0700

    commit 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	int a = '0';
+	int a;
 
-	while (a <= 9)
+	for (a = 0; a <= 9; a++)
 	{
-		if (a != '2' && a != '4')
-		{
-			_putchar(a);
-		}
-		a++;
+		if ((a == 2) || (a == 4))
+			continue;
+		else
+			_putchar(a + '0');
 	}
 	_putchar('\n');
 
