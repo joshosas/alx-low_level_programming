3da97d9a75fd050a894a83fd40141d5f0a6c2bbc
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-07 14:44:56 -0700

    commit 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0 to 9
+ * 
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar('a');
+		_putchar('\n');
+	}
+
+}
