ccae36438cd26a235525b750b4973282a96ae642
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-07 21:28:59 -0700

    commit 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function calculates largest prime factor
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int x = 612852475143;
+	long int pf;
+
+	for (pf = 2; pf < x; pf++)
+	{
+		if (x % pf == 0)
+		{
+			x = x / pf;
+		}
+	}
+	printf("%ld\n", pf);
+	return (0);
+}
