3c001d03088dbff56cda197187ff8b3265506d3a
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-12 08:13:19 -0700

    3rd commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,15 @@
  * _strlen - function calculates length of characters in a string
  * @s: pointer to char
  * Return: length of characters
- */
+*/
 int _strlen(char *s)
 {
-       int len = 0;
-
-       while (*s != '\0')
-       {
-	       s++;
-	       len++;
-       }
-       return (len);
+	int len = 0;
+	
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	return (len);
 }
