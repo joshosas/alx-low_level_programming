3fe16e037e2dadbdcb8019ec5240edbfd412d12d
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-14 05:47:10 -0700

    2nd commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strncat - function that concatenates srtings
+ * @dest: pointer to 1st char
+ * @src: pointer to 2nd char
+ * @n: integer parameter
+ * Return: return value of *dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j = 0;
+
+	for (i = 0; i <= 999; i++)
+	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		j++;
+	}
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
