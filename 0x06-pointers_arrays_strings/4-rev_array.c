970ce2b84070255181f8ee2872569b0bcef115b3
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-14 07:20:58 -0700

    5th commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - function reverses elements of an array
+ * @a: pointer to parameter
+ * @n: number of array elements
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j = 0;
+	int rev;
+
+	for (i = 0; i < n; i++)
+	{
+		rev = a[i];
+		a[i] = a[j];
+		a[j] = rev;
+		j--;
+	}
+}
