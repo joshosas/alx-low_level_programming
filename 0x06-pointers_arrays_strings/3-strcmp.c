9358f41fb21fb67f314cf5fa912f02c3bf0546b4
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-14 06:27:23 -0700

    4th commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - function compares strings
+ * @s1: 1st parameter
+ * @s2: 2nd paraneter
+ * Return: 0 or 1
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (s1 == s2);
+}
