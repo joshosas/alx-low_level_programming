c6b023b3f0e45f4e7a20b330f18d5c300fa2f39b
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-14 07:00:17 -0700

    4th[i + 1] commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	return (s1 == s2);
+	int i, x;
+
+	for (i = 0; s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'); i++)
+	{
+	}
+	x = s1[i] - s2[i];
+	return (x);
 }
