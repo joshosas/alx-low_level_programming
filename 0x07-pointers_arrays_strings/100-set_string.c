ca9a03b4f7e278818c149d20e5afdf275f877f0a
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-18 17:53:46 -0700

    Midnight work

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - function sets string
+ * @s: pointer to a pointer
+ * @to: pointer
+ * Return: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
