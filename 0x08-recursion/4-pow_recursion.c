969fcf4346f40816b55c3d95285137577e31c7cf
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-20 07:46:56 -0700

    Did it work :)

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function prints the factorial of a number
+ * @x: integer
+ * @y: integer
+ *
+ * Return: int
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x,  y - 1));
+}
+
