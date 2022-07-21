58da3ae9c31af506aa7eb3d3f8e5068427104f72
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-21 13:16:55 -0700

    betty fixed after missing deadline (:

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,13 +3,13 @@
 /**
  * wildcmp - function compares string
  * @s1: pointer
- * @s: pointer
+ * @s2: pointer
  * Return: 0
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	if(*s1 == '\0')
+	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
