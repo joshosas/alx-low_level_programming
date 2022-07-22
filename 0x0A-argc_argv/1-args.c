83b5d5fa55ce790383d5f0dc33097ae3b8843ee6
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-07-22 15:37:16 -0700

    commit 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - function
+ * main - function prints number of arguments
  * @argc: argument count
  * @argv: argument vector
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc);
 	return (0);
