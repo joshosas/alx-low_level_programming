6799f3dc9fb28e57c9252f66be3e76d28548bed8
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-08-02 08:46:17 -0700

    3 files upload

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef FILE_MACRO
+#define FILE_MACRO
+#define PI 3.14159265359
+
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * *main - checks code for File's name
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	print("%s\n", _FILE_);
+	return (0);
+}
