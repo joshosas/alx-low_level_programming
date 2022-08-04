ea2718e8cd89e10859e648348a054905e2d9f55e
Joshua Aigbiremhon <joshuaosaro20@gmail.com>
2022-08-04 16:40:58 -0700

    commit 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all parameters
+ * @n: args
+ * Return: sum or if n == 0, return 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i, sum = 0;
+
+	if (n)
+	{
+		va_start(ap, n);
+		for (i = 0; i < n; i++)
+		{
+			sum += va_arg(ap, int);
+		}
+		va_end(ap);
+	}
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is a README file containing all assignments in the variadic Functions Projects
